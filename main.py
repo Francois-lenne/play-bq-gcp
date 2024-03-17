5a7fa754d751f3d59dcc0f73c118b17f77436001
François Lenne <francois.lenne@hotmail.fr>
2024-03-17 17:59:13 +0100

    debug the 258 line

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ def main(request):
     df_game_filtered = game_need_update(time_play_df, df_game)
 
 
-    target_table_id = f"{get_project_id()}.{get_secret(get_project_id(), "DATASET_NAME")}.{get_secret(get_project_id(), "TABLE_NAME_GAME")}"
+    target_table_id = f"{get_project_id()}.{get_secret(get_project_id(), 'DATASET_NAME')}.{get_secret(get_project_id(), 'TABLE_NAME_GAME')}"
 
     temp_table_id = f"{get_project_id()}.{get_secret(get_project_id(), "DATASET_NAME")}.temp_table"
 
